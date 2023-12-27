e9765aab6c53f88942c5aa3e86e849a0d29dbc17
AndreyMustafin2001 <andrey.must2001@gmail.com>
2023-12-27 22:33:50 +0400

    isDetonated function added

--- bombs.cpp
+++ bombs.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 
 struct Bomb{
- int x;
- int y;
- int r;
+    int x;
+    int y;
+    int r;
 
- Bomb(int a,int b,int c)
- {
-  x=a;
-  y=b;
-  r=c;
- }
+    Bomb(int a,int b,int c)
+    {
+    x=a;
+    y=b;
+    r=c;
+    }
 };
+bool isDetonated(Bomb& bomb1,Bomb& bomb2)
+{
+    int squaredDist = (bomb1.x - bomb2.x)*(bomb1.x - bomb2.x) + (bomb1.y - bomb2.y)*(bomb1.y - bomb2.y);
+    if(squaredDist <= bomb1.r * bomb1.r)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 int main()
 {
+    Bomb bomb1(2,1,3);
+    Bomb bomb2(6,1,4);
 
- Bomb bomb1(2,1,3);
-
- return 0;
+    if(isDetonated(bomb1,bomb2))
+        std::cout <<"true";
+    else
+        std::cout <<"true";
 }
-
